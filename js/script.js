d8e63c9f5bb0979e2d07a8240804c619d2b0a1b7
Leandra Aikins <leandra.aikins@gmail.com>
2018-11-13 23:24:55 +0000

    var

--- js/script.js
+++ js/script.js
@@ -8,4 +8,30 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+var userChoice;
+var computerChoice;
+var winner;
 
+   
+$("#shoot").click(function(){
+    userChoice = $("#input").val();
+    
+    $("#userChoice").text(userChoice);
+    var randomNumber = Math.random();
+    
+     console.log(randomNumber);
+    if(randomNumber < .33) {
+        $("#computerChoice").text("rock"); 
+    } else if(randomNumber > .33 && randomNumber < .66){
+        $("#computerChoice").text("scissor")
+    }else{
+    $("#computerChoice").text("paper");
+}
+
+});
+    
+    
+    
+    
+    
+    
